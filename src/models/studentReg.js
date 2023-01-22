751a8f24b932854d1d76919bc1985704ecadc34d
Jayveer <jaay148635@gmail.com>
2023-01-22 15:16:11 +0530

    Updated Student Schema.

---
+++ src/models/studentReg.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    gender: {
+        type: String,
+        required: true
+    },
+
+    phoneNumber: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+
+    age: {
+        type: Number,
+        required: true
+    },
+
+    batch: {
+        type: Number,
+        required: true
+    },
+
+    cpi: {
+        type: Number,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    confirmPassword: {
+        type: String,
+        required: true
+    }
+});
+
+
+//Creating a collection
+const studentRegistration = new mongoose.mongoose.model("studentRegistration", studentSchema);
+
+module.exports = studentRegistration;
