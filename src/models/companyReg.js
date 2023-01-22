349d2f4db6c554bc49fc0b7e421d7375514e1217
Jayveer <jaay148635@gmail.com>
2023-01-22 15:16:27 +0530

    Updated Company Schema.

---
+++ src/models/companyReg.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const companySchema = new mongoose.Schema({
+    companyName: {
+        type: String,
+        required: true
+    },
+
+    officialWebsite: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    jobPosition: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    batch: {
+        type: Number,
+        required: true
+    },
+
+    requiredcpi: {
+        type: Number,
+        required: true
+    },
+
+    package: {
+        type: Number,
+        required: true
+    },
+
+    jobDescription: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    confirmPassword: {
+        type: String,
+        required: true
+    }
+});
+
+//Creating a collection
+const companyRegistration = new mongoose.mongoose.model("companyRegistration", companySchema);
+
+module.exports = studentRegistration;
